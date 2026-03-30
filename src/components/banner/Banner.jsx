e2f7ed1a4e71ca4b562a249f385fa9a6ebfeee8b
GrRabby <grrabby9@gmail.com>
2026-03-30 11:40:43 +0600

    Added Nav & Banner

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import circle from "../../assets/circle.png"
+import play from "../../assets/play.png"
+import bannerImg from "../../assets/banner.png"
+const Banner = () => {
+    return (
+        <div className='flex justify-center items-center gap-15 max-w-400 mx-auto py-[85px]'>
+            <div className='flex flex-col justify-start items-start gap-4 max-w-180'>
+                <div className="px-4 py-2 bg-[#E1E7FF] rounded-[1000px] text-[16px] font-medium">
+                    <span className="bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent flex gap-2 justify-center items-center">
+                        <img src={circle} alt="" /> New: AI-Powered Tools Available
+                    </span>
+                </div>
+                <h2 className='font-extrabold text-[72px] text-[#101727]'>Supercharge Your Digital Workflow</h2>
+                <p className='text-[18px] text-[#627382]'>Access premium AI tools, design assets, templates, and productivity<br></br>
+                software—all in one place. Start creating faster today.<br></br>
+                Explore Products</p>
+                <div className='flex justify-center items-center gap-4 mt-4'>
+                    <button className='btn btn-primary py-3 px-4 font-semibold rounded-[10000px] bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white border-none'>Explore Products</button>
+                    <button className='btn btn-primary shadow-none not-first-of-type:py-3 px-4 font-semibold rounded-[10000px] bg-white'>
+                        <span className="bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent flex justify-center items-center gap-2">
+                           <img src={play} alt="" />Watch Demo
+                        </span>
+                    </button>
+                </div>
+            </div>
+            <div>
+                <img src={bannerImg} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import NavLogo from "../../assets/DigiTools.png"
+import CartLogo from "../../assets/Cart.png"
+const Navbar = () => {
+    return (
+        <>
+            <div className=' border border-[#F2F2F2] py-6.5 flex justify-center items-center '>
+                <div className="navbar bg-base-100 max-w-400 mx-auto">
+                    <div className="navbar-start">
+                        <img src={NavLogo} alt="" />
+                    </div>
+                    <div className="navbar-center hidden lg:flex">
+                        <ul className="menu menu-horizontal px-1 gap-6 text-[16px]">
+                            <li>Products</li>
+                            <li>Features</li>
+                            <li>Pricing</li>
+                            <li>Testimonials</li>
+                            <li>FAQ</li>
+                        </ul>
+                    </div>
+                    <div className="navbar-end">
+                        <div className='flex justify-center items-center gap-4'>
+                            <img src={CartLogo} alt="" />
+                            <p>Login</p>
+                            <button className='btn btn-primary py-3 px-4 font-semibold rounded-[10000px] bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white border-none'>Get Started</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </>
+    );
+};
+
+export default Navbar;
